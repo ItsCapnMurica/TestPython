accf2c37216b3c9e943a7a134dfeca69e4964ddc
ItsCapnMurica <landon.t.weddle@gmail.com>
2026-04-25 18:12:21 -0400

    Added a rectangle area calculator (Works with any polygon)

---
+++ rectangle.py
@@ -0,0 +1,6 @@
+# Calculate the area of a rectangle
+
+length = float(input("What is the length of your rectangle in cm?: "))
+width = float(input("What is the width of your rectangle in cm?: "))
+area = length * width
+print(f"The area of your rectangle is {area}cm")
