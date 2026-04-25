727364b53dbf9e0cbf5b43ea7689652799dc065c
ItsCapnMurica <landon.t.weddle@gmail.com>
2026-04-25 17:42:30 -0400

    Actually added stuff this time

---
+++ main.py
@@ -0,0 +1,9 @@
+# Very Simple Madlibs
+
+adjective1 = input("Name an adjective: ")
+adjective2 = input("Name an adjective: ")
+noun1 = input("Name a noun: ")
+noun2 = input("Name a noun: ")
+
+print(f"My {noun1} is {adjective1}")
+print(f"I {adjective2} my {noun2}")
