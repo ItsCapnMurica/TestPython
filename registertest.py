e0f8d255d359b977a0a7b3e74c5d3fd5b56c0479
ItsCapnMurica <landon.t.weddle@gmail.com>
2026-04-26 21:32:10 -0400

    Tested adding an app registering test (enter a number or the file fails)

---
+++ registertest.py
@@ -0,0 +1,10 @@
+# register test
+
+age = int(input("What is your age? Enter a number: "))
+
+if age >= 18:
+    print("You are able to register!")
+elif age < 18:
+    print("You are too young.")
+else:
+    print("????")
