6c55ed0712a63cd06d26c28a060d879bc866a8b4
ItsCapnMurica <landon.t.weddle@gmail.com>
2026-04-27 17:52:04 -0400

    added sqare roots

---
+++ sqrt.py
@@ -0,0 +1,8 @@
+import math
+
+# find the sqare root of a number
+
+num = float(input("What number would you like to know the square root of?: "))
+sqrt = math.sqrt(num)
+
+print(f"The square root is {round(sqrt, 3)}")
